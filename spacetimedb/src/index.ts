439dd0f139aa99296bca80c5fce6fd712929de86
DLineone <ancientcolosus@gmail.com>
2026-03-08 01:04:54 +0300

    add set name reducer

--- spacetimedb/src/index.ts
+++ spacetimedb/src/index.ts
@@ -22,20 +22,25 @@ const message = table(
 const spacetimedb = schema({ user, message });
 export default spacetimedb;
 
-export const onDisconnect = spacetimedb.clientDisconnected(_ctx => {
-  // Called every time a client disconnects
-});
+function validateName(name: string) {
+  if (!name) {
+    throw new SenderError("Names must not be empty");
+  }
+  if (name.length < 3 || name.length > 20) {
+    throw new SenderError(
+      "Names must not be shorter than 3 and longer than 20",
+    );
+  }
+}
 
-export const add = spacetimedb.reducer(
+export const set_name = spacetimedb.reducer(
   { name: t.string() },
   (ctx, { name }) => {
-    ctx.db.person.insert({ name });
-  }
+    validateName(name);
+    const user = ctx.db.user.identity.find(ctx.sender);
+    if (!user) {
+      throw new SenderError("Cannot set name for unknown user");
+    }
+    ctx.db.user.identity.update({ ...user, name });
+  },
 );
-
-export const sayHello = spacetimedb.reducer(ctx => {
-  for (const person of ctx.db.person.iter()) {
-    console.info(`Hello, ${person.name}!`);
-  }
-  console.info('Hello, World!');
-});
